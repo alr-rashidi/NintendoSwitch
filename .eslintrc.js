6b6f141693d5611a8ce9890c70b1731480995754
Alireza <alr.rashidi@outlook.com>
2024-04-05 15:59:46 +0330

    🐳 chore: install prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,34 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "rules": {
-                "@typescript-eslint/no-var-requires": "off"
-            },
-            "files": [
-                ".eslintrc.{js,cjs}",
-                "webpack.config.js"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "react"
-    ],
-    "rules": {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off'
+      },
+      files: ['.eslintrc.{js,cjs}', 'webpack.config.js'],
+      parserOptions: {
+        sourceType: 'script'
+      }
     }
-}
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {}
+};
